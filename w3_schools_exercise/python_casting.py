401d6e640463a25412e412e4e2e77eb89cae4557
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 16:11:08 +0800

    manual data type declaration

---
+++ w3_schools_exercise/python_casting.py
@@ -0,0 +1,13 @@
+# Specify a Variable Type
+
+x = int(2)
+print(x, "is a", type(x))
+
+y = float(45)
+print(y, "is a", type(y))
+
+z = complex(3)
+print(z, "is a", type(z))
+
+a = str("Hello")
+print(a, "is a", type(a))
