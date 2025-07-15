0ddc179a7dfce8f3993f6f820ffef59a6ebc12c3
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:40:17 +0800

    # Start with the number 50, add 30 to it using the += operator, and print the new value.

---
+++ day_1/problem_42.py
@@ -0,0 +1,6 @@
+# Start with the number 50, add 30 to it using the += operator, and print the new value.
+
+def add_value(number, added):
+    number += added
+    return number
+print(add_value(50, 30))
