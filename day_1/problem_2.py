0901b3174592c37cbe3cd112480f0dd5fc109c07
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:32:28 +0800

    # Create two variables and concatenate like your first name and a last name.

---
+++ day_1/problem_2.py
@@ -0,0 +1,10 @@
+# Create two variables and concatenate like your first name and a last name.
+
+def print_age_name():
+    first_name = "Renz"
+    last_name = "Mapa"
+    
+    full_name = first_name + " " + last_name
+    return full_name
+
+print(print_age_name())
