8c1cb6139d86ea2e1a442fe7526e816a2d7f7a6d
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:34:55 +0800

    # Create two variables with any words and print them combined in one sentence.

---
+++ day_1/problem_18.py
@@ -0,0 +1,10 @@
+# Create two variables with any words and print them combined in one sentence.
+
+word_one = "hello!!!"
+word_two = "sayoooo"
+
+
+def print_sentence(word_one, word_two):
+    print(str(word_one), str(word_two))
+    
+print_sentence(word_one, word_two)
