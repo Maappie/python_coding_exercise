7e081b85836b662b364396a5bc16f9c8b33f671d
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:33:33 +0800

    # Create a variable for a favorite food and print it three times.

---
+++ day_1/problem_9.py
@@ -0,0 +1,9 @@
+# Create a variable for a favorite food and print it three times.
+
+def print_food(food):
+    for _ in range(3):
+        print(food)
+        
+favorite_food = "chicken"
+
+print_food(favorite_food)
