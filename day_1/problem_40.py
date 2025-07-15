66f2436789b3574453fcc900f1eabd2b55770b7a
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:39:58 +0800

    # Find the remainder when 29 is divided by 4 and print it.

---
+++ day_1/problem_40.py
@@ -0,0 +1,6 @@
+# Find the remainder when 29 is divided by 4 and print it.
+
+def remainder(dividend, divisor):
+    return dividend % divisor
+
+print(f"The remainder for 29/4 is {remainder(29, 4)}")
