a86a265f65cb96f651d1f6fa84f36f4b6a7216cf
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:33:42 +0800

    # Subtract two numbers and store the result. Print the sentence: “The difference is ___.”

---
+++ day_1/problem_10.py
@@ -0,0 +1,10 @@
+# Subtract two numbers and store the result. Print the sentence: “The difference is ___.”
+
+def subtract_numbers(num_1, num_2):
+    total = num_1 - num_2
+    return total
+
+num_1 = 20
+num_2 = 10
+
+print("the difference is " + str(subtract_numbers(num_1, num_2)))
