504939f24b58e18ea98940a96688ea52b96d7ec5
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:40:52 +0800

    # Using the number 8, multiply it by 5 with the *= operator, then print the result.

---
+++ day_1/problem_44.py
@@ -0,0 +1,7 @@
+# Using the number 8, multiply it by 5 with the *= operator, then print the result.
+
+def multiply_value(number, multiplied):
+    number *= multiplied
+    return number
+
+print(multiply_value(8, 5))
