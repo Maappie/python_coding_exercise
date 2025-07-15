3234210ecfc1449fe4ac4aab596784931501d123
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:33:53 +0800

    # Combine a greeting and a name into a single print statement using variables.

---
+++ day_1/problem_11.py
@@ -0,0 +1,9 @@
+# Combine a greeting and a name into a single print statement using variables.
+
+def greet_name(greet, name):
+    print(str(greet) + ", " + str(name))
+
+greet = "Hello good morning"
+name = "Renz"
+
+greet_name(greet, name)
