13c51d61328d1437afa590168297ae5d924eafb6
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 04:37:16 +0800

    ways of copying list in different methods

---
+++ w3_schools_exercise/copy_list.py
@@ -0,0 +1,19 @@
+# use copy method to copy list
+fruits = [ "Orange", "Kiwi", "cherry", "orange", "kiwi", "banana"]
+new_fruits = fruits.copy()
+
+print(new_fruits)
+
+#use list method to create a copy of a list
+fruits = [ "Orange", "Kiwi", "cherry", "orange", "kiwi", "banana"]
+new_fruits = list(fruits)
+
+print(new_fruits)
+
+# use slice method to copy
+fruits = [ "Orange", "Kiwi", "cherry", "orange", "kiwi", "banana"]
+new_fruits = fruits[:]
+
+print(new_fruits)
+
+
