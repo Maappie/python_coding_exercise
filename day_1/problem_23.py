95dd2e88d954982c62ce9136cddaab7baf3943ea
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:35:38 +0800

    # Create two string variables: one for a book title and one for the author. Print a sentence about the book.

---
+++ day_1/problem_23.py
@@ -0,0 +1,9 @@
+# Create two string variables: one for a book title and one for the author. Print a sentence about the book.
+
+book_title = "Dora the explorer"
+author = "Diego"
+
+def print_sentence(title, author):
+    print("The book called", book_title, "was written by", author)
+    
+print_sentence(book_title, author)
