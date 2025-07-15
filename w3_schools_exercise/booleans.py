a2eb2691b9a4af03212d347e3584484019fe1944
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 18:04:05 +0800

    how booleans works in python

---
+++ w3_schools_exercise/booleans.py
@@ -0,0 +1,25 @@
+# declare a value and set to true or false
+value = True
+value_second = False
+print(value)
+print(value_second, "\n")
+
+# this will return a true or false value
+print(15 > 7)
+print(15 == 7)
+print(15 < 7)
+print(15 >= 7)
+print(15 <= 7)
+print(bool("Hellso"))
+print(bool(15))
+print(bool(), "\n")
+
+# functions can also return true or false
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+  
