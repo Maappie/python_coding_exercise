8e566ccac1534e02200f89daba944c9ac6351b73
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 16:02:37 +0800

    number data types and conversion

---
+++ w3_schools_exercise/python_numbers.py
@@ -0,0 +1,28 @@
+x = 10
+y = 10.52
+z = 4j
+print(x ,"is a", type(x))
+print(y, "is a", type(y))
+print(z, "is a", type(z))
+print("\n")
+
+# convert to float
+x_float = float(x)
+y_float = float(y)
+print(x_float ,"is a", type(x_float))
+print(y_float, "is a", type(y_float))
+print("\n")
+
+# convert to int
+x_int = int(x)
+y_int = int(y)
+print(x_int ,"is a", type(x_int))
+print(y_int, "is a", type(y_int))
+print("\n")
+
+# convert to complex
+x_complex = complex(x)
+y_complex = complex(y)
+print(x_complex ,"is a", type(x_complex))
+print(y_complex, "is a", type(y_complex))
+print(z, "is a", type(z))
