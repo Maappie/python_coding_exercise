16e7c8e72dee2ffa3594fe53c47575ee624fd01b
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:32:46 +0800

    # Ask for a user’s name and print a greeting including their name.

---
+++ day_1/problem_4.py
@@ -0,0 +1,9 @@
+# Ask for a user’s name and print a greeting including their name.
+
+def print_name(name):
+    print("Your name is " + name)
+    return
+
+name = input("What is your name?")
+
+print_name(name)
