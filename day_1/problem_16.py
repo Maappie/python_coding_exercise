aba1facab2dbfe8091ad65d874bd7438c324062f
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:34:39 +0800

    # Concatenate three strings: your name, hobby, and favorite color, and print them.

---
+++ day_1/problem_16.py
@@ -0,0 +1,10 @@
+# Concatenate three strings: your name, hobby, and favorite color, and print them.
+
+def print_sentence(name, hobby, favorite_color):
+    print(str(name), "hobby is", str(hobby), "and his favorite color is", str(favorite_color))
+
+name = "Renz"
+hobby = "watching movies"
+favorite_color = "red"
+
+print_sentence(name, hobby, favorite_color)
