0f2651128807b676e246c12d436dbcea03d5b6f8
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:40:36 +0800

    # Begin with the number 100, subtract 25 from it using the -= operator, and display the result.

---
+++ day_1/problem_43.py
@@ -0,0 +1,7 @@
+# Begin with the number 100, subtract 25 from it using the -= operator, and display the result.
+
+def subtract_value(number, subtracted):
+    number -= subtracted
+    return number
+
+print(subtract_value(100, 25))
