949fd81f742ab00c913b3ee3e5ba126089709b10
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-15 14:34:12 +0800

    # Create a variable for a fruit name, and print a sentence saying, “I love to eat [fruit].”

---
+++ day_1/problem_13.py
@@ -0,0 +1,8 @@
+# Create a variable for a fruit name, and print a sentence saying, “I love to eat [fruit].”
+
+def print_sentence(fruit):
+    print("I love to eat " + str(fruit))
+
+fruit = "Apple"
+
+print_sentence(fruit)
