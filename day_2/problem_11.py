891be254f8c87fbb46e75b31ae2a19d432cbb996
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 14:32:16 +0800

    # Create an array with the names of 4 family members. Print the last name in the array.

---
+++ day_2/problem_11.py
@@ -0,0 +1,9 @@
+# Create an array with the names of 4 family members. Print the last name in the array.
+
+names = ["mapa", "renz", "daneco", "dideles"]
+
+def print_last(list):
+    print(list[-1])
+    
+print_last(names)
+
