219ac3e7019ab58443088d49944557630926ec3e
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 14:31:55 +0800

    # You have an array of students’ scores, Find the highest score in the array and print it.

---
+++ day_2/problem_8.py
@@ -0,0 +1,9 @@
+# You have an array of students’ scores, Find the highest score in the array and print it.
+
+scores = [85, 90, 78, 92, 88]
+
+def find_max(list):
+    result = max(list)
+    return result
+
+print(find_max(scores))
