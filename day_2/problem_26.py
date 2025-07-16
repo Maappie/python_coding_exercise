7d3cf91cea4c898efd0880b5658e3a0c415903d0
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 14:34:19 +0800

    # Print all the keys in the dictionary.

---
+++ day_2/problem_26.py
@@ -0,0 +1,9 @@
+# Print all the keys in the dictionary.
+
+
+numbers = { "one": 1, "two": 2, "three": 3 }
+
+def show_all_keys(dictionary):
+    return  list(dictionary.keys())
+
+print(show_all_keys(numbers))
