e203ecdf402c6c5702314c5d377a1fda7184568d
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 14:31:02 +0800

    # Create an array that contains the days of the week, from Monday to Sunday. Print the third day in the array.

---
+++ day_2/problem_3.py
@@ -0,0 +1,7 @@
+# Create an array that contains the days of the week, from Monday to Sunday. Print the third day in the array.
+
+def print_a_day(list, index):
+    print(list[index])
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print_a_day(days, 2)
