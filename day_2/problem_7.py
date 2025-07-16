3ca930828fb9a9823ef0cda1fd90ff0de15c66b8
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 14:31:41 +0800

    # Create an array of your 5 favorite movies. Print the length of the array.

---
+++ day_2/problem_7.py
@@ -0,0 +1,9 @@
+# Create an array of your 5 favorite movies. Print the length of the array.
+
+def list_length(list):
+    length = len(list)
+    return length
+
+movies = ["movie 1", "movie 2", "movie 3", "movie 4", "movie 5"]
+
+print(list_length(movies))
