b93fef2949bd9cec60cfa3591db13ee94560d2f3
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-16 14:31:23 +0800

    # Given the following array, sort the array in ascending order and print the result.

---
+++ day_2/problem_5.py
@@ -0,0 +1,10 @@
+# Given the following array, sort the array in ascending order and print the result.
+
+def sort_list(list):
+    list.sort()
+    return list
+
+numbers = [2, 5, 3, 8, 1]
+
+numbers = sort_list(numbers)
+print(numbers)
