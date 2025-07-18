bd6c68947d7157f95a3b2be16b8480684eeefe16
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-18 20:56:14 +0800

    # Create a program that prints the square of numbers from 1 to 10 using the times method.

---
+++ day_4/problem_4.py
@@ -0,0 +1,8 @@
+# Create a program that prints the square of numbers from 1 to 10 using the times method.
+
+def loop():
+    for x in range(10):
+        x += 1
+        print(f"{x * x}")
+
+loop()
