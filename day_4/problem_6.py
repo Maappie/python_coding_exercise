47eacb5b71f187e0d1203b5697862f4c300d5b59
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-18 21:03:25 +0800

    # Write a Python program that prints the sum of numbers from 1 to 100 using loop.

---
+++ day_4/problem_6.py
@@ -0,0 +1,13 @@
+# Write a Python program that prints the sum of numbers from 1 to 100 using loop.
+
+def loop():
+    numbers = 100
+    result = 0
+    for x in range(100):
+        result += (x+1)
+    return result
+
+print(loop())
+
+        
+
