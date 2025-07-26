37295df8bc508f4288463d58eb8036dc4d427650
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-26 10:17:41 +0800

    # Create an array of colors and print each color.

---
+++ day_4/problem_15.py
@@ -0,0 +1,9 @@
+# Create an array of colors and print each color.
+
+colors = ["red", "blue", "green"]
+
+def show_colors():
+    for x in range(len(colors)):
+        print(colors[x])
+        
+show_colors()
