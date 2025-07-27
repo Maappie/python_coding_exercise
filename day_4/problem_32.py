d24c023b0c14b6f8344ff203947639378ad29ef4
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-07-27 14:08:28 +0800

    # Create an array of strings and use a for loop to print each string.

---
+++ day_4/problem_32.py
@@ -0,0 +1,8 @@
+# Create an array of strings and use a for loop to print each string.
+
+fruits = ["apple", "banana", "cherry"]
+
+def show_fruits():
+    for x in fruits:
+        print(x)
+show_fruits()
